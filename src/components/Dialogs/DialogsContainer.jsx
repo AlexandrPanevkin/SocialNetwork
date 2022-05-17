d12facba72d418ad981655163b35f427be53a82c
AlexanderPanevkin <panevkin900@gmail.com>
2022-05-17 20:16:00 +0300

    StoreContext

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -3,25 +3,32 @@ import {
   sendMessageCreator,
   updateMewMessageTextCreator,
 } from "../../redux/dialogs-reducer";
+import StoreContext from "../../StoreContext";
 import Dialogs from "./Dialogs";
 
-const DialogsContainer = (props) => {
-  let state = props.store.getState().dialogsPage;
-
-  let onSendMessageClick = () => {
-    props.store.dispatch(sendMessageCreator());
-  };
-
-  let onNewMessageChange = (newMessageText) => {
-    props.store.dispatch(updateMewMessageTextCreator(newMessageText));
-  };
-
+const DialogsContainer = () => {
   return (
-    <Dialogs
-      updateNewMessageText={onNewMessageChange}
-      sendMessage={onSendMessageClick}
-      dialogsPage={state}
-    />
+    <StoreContext.Consumer>
+      {(store) => {
+        let state = store.getState().dialogsPage;
+
+        let onSendMessageClick = () => {
+          store.dispatch(sendMessageCreator());
+        };
+
+        let onNewMessageChange = (newMessageText) => {
+          store.dispatch(updateMewMessageTextCreator(newMessageText));
+        };
+
+        return (
+          <Dialogs
+            updateNewMessageText={onNewMessageChange}
+            sendMessage={onSendMessageClick}
+            dialogsPage={state}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 
