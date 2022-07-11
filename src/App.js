2d33d462506e30e8b2b64e610716f219152ee0e5
AlexanderPanevkin <panevkin900@gmail.com>
2022-07-11 20:35:13 +0300

    add Lazy, Suspens

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, {Component} from "react";
+import React, { Component, Suspense } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import { compose } from "redux";
 import { initializeApp } from "./redux/app-reducer";
 import { connect } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
+const UsersContainer = React.lazy(() =>
+  import("./components/Users/UsersContainer")
+);
 
 class App extends Component {
   componentDidMount() {
@@ -25,27 +32,27 @@ class App extends Component {
     }
     return (
       <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/dialogs" element={<DialogsContainer />} />
-            <Route path="/profile" element={<ProfileContainer />} />
-            <Route path="/profile/:userId" element={<ProfileContainer />} />
-            <Route path="/users" element={<UsersContainer />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/login" element={<LoginPage />} />
+        <Suspense fallback={<div>Загрузка...</div>}>
+          <HeaderContainer />
+          <Navbar />
+          <div className="app-wrapper-content">
+            <Routes>
+              <Route path="/dialogs" element={<DialogsContainer />} />
+              <Route path="/profile" element={<ProfileContainer />} />
+              <Route path="/profile/:userId" element={<ProfileContainer />} />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/music" element={<Music />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/login" element={<LoginPage />} />
             </Routes>
-        </div>
+          </div>
+        </Suspense>
       </div>
     );
-  } 
+  }
 }
 const mapStateToProps = (state) => ({
-  initialized: state.app.initialized
-
-})
-export default compose( 
-  connect(mapStateToProps, { initializeApp }))(App);
+  initialized: state.app.initialized,
+});
+export default compose(connect(mapStateToProps, { initializeApp }))(App);
